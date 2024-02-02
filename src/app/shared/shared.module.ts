4c8da50539b2287891770f3e4cb75662caf537f3
bryanlema <bryanglema2@gmail.com>
2024-02-01 21:20:56 -0500

    Estructura del proyecto

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    Error404PageComponent
+  ],
+  exports: [
+    Error404PageComponent
+  ]
+})
+export class SharedModule { }
